48ba42666cc526d3bfd999e382473f364ee7237c
Filip Sjölander <filip.sjolander@gmail.com>
2025-07-04 16:59:05 +0200

    new init

--- src/routes/cv.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/cv')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/cv"!</div>
-}
